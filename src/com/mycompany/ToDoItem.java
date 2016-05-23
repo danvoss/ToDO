0b636e33946b9466448548d6c9b85a147290180f
Dan <danielandrewvoss@gmail.com>
2016-05-23 11:16:42 -0400

    Add ToDo

---
+++ src/com/mycompany/ToDoItem.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+/**
+ * Created by Dan on 5/23/16.
+ */
+public class ToDoItem {
+
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
